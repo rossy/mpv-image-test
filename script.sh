617dc2e17c653731b796a22b1f6ecfa23ea51f6c
James Ross-Gowan <rossy@jrg.systems>
2017-07-07 20:40:24 +1000

    fix/change stuff

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ command_fifo=/tmp/mpv-test-fifo-$$-$RANDOM
 trap 'rm -f "$command_fifo"' EXIT INT TERM HUP
 mkfifo -m 600 "$command_fifo"
 
-mpv --no-config --no-osc --idle --input-file="$command_fifo" &
+mpv --no-config --no-osc --no-hidpi-window-scale --idle --input-file="$command_fifo" &
 mpv_pid=$!
 
 printf >>"$command_fifo" 'set pause yes\n'
@@ -20,7 +20,8 @@ printf >>"$command_fifo" 'quit\n'
 wait $mpv_pid
 psnr=$(compare 2>&1 >/dev/null -metric PSNR test-001-actual.png test-001-expected.png test-001-diff.png || true)
 
-if [[ $psnr == 'inf' ]] || [[ $(echo "$psnr > $psnr_threshold" | bc) == '1' ]]; then
+if [[ $psnr == 'inf' ]] || [[ $psnr == '0' ]] \
+     || [[ $(echo "$psnr > $psnr_threshold" | bc) == '1' ]]; then
 	printf 'success (PSNR: %s)\n' "$psnr"
 	exit 0
 else
