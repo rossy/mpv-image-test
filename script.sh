45042c6051d75f7029c379f8ac1a7fb02a688893
James Ross-Gowan <rossy@jrg.systems>
2017-07-07 22:59:39 +1000

    Add CC0 licence

--- script.sh
+++ script.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty. See
+# <https://creativecommons.org/publicdomain/zero/1.0/> for a copy of the CC0
+# Public Domain Dedication, which applies to this software.
 set -e
 
 psnr_threshold=50
